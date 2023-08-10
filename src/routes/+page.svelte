d6eed6d166481a33c785c42db5facd0c7a4e814e
Cauê Henrique <caue313@gmail.com>
2023-08-10 09:10:48 -0300

    Add: notebooks

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -54,6 +54,36 @@
             title: "GAMER I7 3 GERAÇÃO",
             image: "gabinete1690.png",
             text: "Computador GAMER!\n\n💥 LEAGUE OF LEGENDS (100FPS+)\n🚔 GTA RP (40/80FPS)\n🌎 MINECRAFT (150FPS+)\n🔫 WARZONE (40/65FPS)\n🦙 FORTNITE (60/120FPS)\n\n*Processador:* i7 3ª GERAÇÃO\n*Memória RAM:* 8GB DDR3\n*Placa de Vídeo:* RX 580 8GB\n*Armazenamento:* SSD 256GB\n*Gabinete:* GAMER 2 FANS RGB NA FRENTE\n*Fonte:* 500W\n\n*R$ 2.690,00* em até 10x sem juros!",
+        },
+        {
+            title: "LENOVO I5-5 8GB SSD256",
+            image: "i5_5_8_256.png",
+            text: "💻 NOTEBOOK LENOVO\n\n*Processador:* i5 5ª GERAÇÃO\n*Memória RAM:* 8GB\n*Armazenamento:* SSD 256GB\n\n*R$ 1.790,00* em até 10x sem juros!"
+        },
+        {
+            title: "LENOVO I5-8 16GB NVME256",
+            image: "i5_8_256.png",
+            text: "💻 NOTEBOOK LENOVO THINKPAD\n\n*Processador:* i5 8ª GERAÇÃO\n*Memória RAM:* 16GB\n*Armazenamento:* NVME 256GB\n\n*R$ 3.290,00* em até 10x sem juros!"
+        },
+        {
+            title: "DELL I7-8 8GB SSD256",
+            image: "i7_8_256.png",
+            text: "💻 NOTEBOOK DELL LATITUDE\n\n*Processador:* i7 8ª GERAÇÃO\n*Memória RAM:* 8GB\n*Armazenamento:* SSD 256GB\n\n*R$ 3.390,00* em até 10x sem juros!"
+        },
+        {
+            title: "LENOVO I7-8 8GB NVME512",
+            image: "i7_8_512.png",
+            text: "💻 NOTEBOOK LENOVO THINKPAD\n\n*Processador:* i7 8ª GERAÇÃO\n*Memória RAM:* 8GB\n*Armazenamento:* NVME 512GB\n\n*R$ 3.690,00* em até 10x sem juros!"
+        },
+        {
+            title: "VOSTRO I5-11 8GB SSD256",
+            image: "i5_11_8.png",
+            text: "💻 NOTEBOOK DELL VOSTRO\n\n*Processador:* i5 11ª GERAÇÃO\n*Memória RAM:* 8GB\n*Armazenamento:* SSD 256GB\n\n*R$ 3.990,00* em até 10x sem juros!"
+        },
+        {
+            title: "VOSTRO I7-10 16GB SSD256",
+            image: "i7_10_16_256.png",
+            text: "💻 NOTEBOOK DELL VOSTRO\n\n*Processador:* i7 10ª GERAÇÃO\n*Memória RAM:* 16GB\n*Armazenamento:* SSD 256GB\n\n*R$ 4.590,00* em até 10x sem juros!"
         }
     ]
 
