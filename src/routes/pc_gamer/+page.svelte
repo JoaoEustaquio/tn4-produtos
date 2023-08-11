3e7047f79c78beb0863d5131bebde76b2d40ab1a
João Eustáquio <joaoeustaquiosg@gmail.com>
2023-08-11 15:53:12 -0300

    Creating pagination to PC Gamer

---
+++ src/routes/pc_gamer/+page.svelte
@@ -0,0 +1,69 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  import CopyButton from "$lib/CopyButton.svelte";
+  import IconCopy from "$lib/icons/copy.svelte";
+
+  const products = [
+    {
+      title: "GAMER 1190",
+      image: "gabinete1690.png",
+      text: "Computador GAMER de ENTRADA!\n\n💥 LEAGUE OF LEGENDS (30/40FPS)\n🌎 MINECRAFT (30/50FPS)\n🟥 ROBLOX (30/50FPS)\n\n*Processador:* i5 3ª GERAÇÃO\n*Memória RAM:* 8GB DDR3\n*Armazenamento:* SSD 256GB\n*Gabinete:* GAMER 2 FANS RGB NA FRENTE\n*Fonte:* 500W\n\n*R$ 1.190,00* em até 10x sem juros!",
+    },
+    {
+      title: "PC GAMER 1990",
+      image: "gabinete1990.png",
+      text: "Computador GAMER de ENTRADA!\n\n💥 LEAGUE OF LEGENDS (60FPS)\n🌎 MINECRAFT (60/70FPS)\n🏎 ROCKET LEAGUE (60/70FPS)\n 🚔 GTA RP (40/80FPS)\n🔫 CSGO/VALORANT (60/90FPS)\n\n*Processador:* i5 3ª GERAÇÃO\n*Memória RAM:* 8GB DDR3\n*Placa de Vídeo:* RX 550 4GB\n*Armazenamento:* SSD 256GB\n*Gabinete:* GAMER 2 FANS RGB NA FRENTE\n*Fonte:* 500W\n\n*R$ 1.990,00* em até 10x sem juros!",
+    },
+    {
+      title: "GAMER 1690",
+      image: "kit1690.png",
+      text: "Computador GAMER de ENTRADA!\n\n💥 LEAGUE OF LEGENDS (30/40FPS)\n🌎 MINECRAFT (30/50FPS)\n🟥 ROBLOX (30/50FPS)\n\n*Processador:* i5 3ª GERAÇÃO\n*Memória RAM:* 8GB DDR3\n*Armazenamento:* SSD 256GB\n*Gabinete:* GAMER 2 FANS RGB NA FRENTE\n*Fonte:* 500W\n\n +*KIT GAMER:* 🖥️ MONITOR SEMINOVO 16 POLEGADAS, ⌨️ TECLADO, 🖱️ MOUSE, MOUSEPAD 🎧 HEADSET\n\n*R$ 1.690,00* em até 10x sem juros!",
+    },
+    {
+      title: "GAMER 1690 + RX 550",
+      image: "gabinete1990.png",
+      text: "Computador GAMER de ENTRADA!\n\n💥 LEAGUE OF LEGENDS (60FPS)\n🌎 MINECRAFT (60/70FPS)\n🟥 ROBLOX (60/70FPS)\n 🚔 GTA RP (40/80FPS)\n🔫 CSGO/VALORANT (60/90FPS)\n\n*Processador:* i5 3ª GERAÇÃO\n*Memória RAM:* 8GB DDR3\n*Placa de Vídeo:* RX 550 4GB\n*Armazenamento:* SSD 256GB\n*Gabinete:* GAMER 2 FANS RGB NA FRENTE\n*Fonte:* 500W\n\n +*KIT GAMER:* 🖥️ MONITOR SEMINOVO 16 POLEGADAS, ⌨️ TECLADO, 🖱️ MOUSE, MOUSEPAD E 🎧 HEADSET\n\n*R$ 2.590,00* em até 10x sem juros!",
+    },
+    {
+      title: "GAMER RYZEN 5 5600G",
+      image: "maquina_ryzen_5.png",
+      text: "Computador GAMER!\n\n💥 LEAGUE OF LEGENDS (100FPS+)\n🚔 GTA RP (40/80FPS)\n🌎 MINECRAFT (150FPS+)\n🔫 WARZONE (40/65FPS)\n🦙 FORTNITE (60/120FPS)\n\n*Processador:* RYZEN 5 5600G\n*Memória RAM:* 16GB DDR4\n*Placa de Vídeo:* VEGA 7 (INTEGRADO)\n*Armazenamento:* SSD 256GB\n*Gabinete:* GAMER 4 FANS RGB NA FRENTE\n*Fonte:* 500W\n\n*R$ 2990,00* em até 10x sem juros!",
+    },
+    {
+      title: "GAMER I7 3 GERAÇÃO",
+      image: "gabinete1690.png",
+      text: "Computador GAMER!\n\n💥 LEAGUE OF LEGENDS (100FPS+)\n🚔 GTA RP (40/80FPS)\n🌎 MINECRAFT (150FPS+)\n🔫 WARZONE (40/65FPS)\n🦙 FORTNITE (60/120FPS)\n\n*Processador:* i7 3ª GERAÇÃO\n*Memória RAM:* 8GB DDR3\n*Placa de Vídeo:* RX 580 8GB\n*Armazenamento:* SSD 256GB\n*Gabinete:* GAMER 2 FANS RGB NA FRENTE\n*Fonte:* 500W\n\n*R$ 2.690,00* em até 10x sem juros!",
+    },
+  ];
+
+  function copyToClipboard(product: { text: string }) {
+    navigator.clipboard.writeText(product.text);
+  }
+</script>
+
+<div class="p-8">
+  <a href="/"> ⬅ Home</a>
+  <h1 class="text-xl font-bold mb-4">⚙️ MÁQUINAS PRONTAS</h1>
+  <div class="grid grid-cols-4 gap-8">
+    {#each products as product}
+      <div class="space-y-4">
+        <img
+          src={product.image}
+          class="rounded-md w-full h-64 object-contain border border-gray-200 bg-gray-50"
+          alt={product.title}
+        />
+        <h1 class="font-bold text-xl truncate">{product.title}</h1>
+        <code
+          class="font-mono-emoji block w-full h-64 overflow-y-scroll whitespace-pre-line bg-gray-100 border border-gray-300 p-2 rounded"
+        >
+          {product.text}
+        </code>
+
+        <CopyButton
+          class="w-full tracking-wider text-sm"
+          on:click={() => copyToClipboard(product)}>COPIAR</CopyButton
+        >
+      </div>
+    {/each}
+  </div>
+</div>
