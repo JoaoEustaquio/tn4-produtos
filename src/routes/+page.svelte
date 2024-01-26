23def3c1f9b8d712eb88667f583fffb13771bc90
Joao Eustaquio <joaoeustaquiosg@gmail.com>
2024-01-26 12:40:34 -0300

    Edit GPU "GTX 1660"

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -20,7 +20,7 @@
         { title: "GTX 1650 4GB VENTUS", price: "R$ 1.290,00" },
         { title: "RX 570 8GB", price: "R$ 1.390,00" },
         { title: "RX 580 8GB", price: "R$ 1.590,00" },
-        { title: "GTX 1660 6GB", price: "R$ 1.790,00" },
+        { title: "GTX 1660 SUPER 6GB", price: "R$ 1.790,00" },
         { title: "RTX 3050 8GB", price: "R$ 1.990,00" },
         { title: "RTX 3060 8GB PEGASUS", price: "R$ 2.390,00" },
         { title: "RTX 4060 8GB", price: "R$ 2.790,00" },
