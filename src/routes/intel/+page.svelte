dc97128a7ead994ec5a49d62a5c83a206f8ecc3f
Joao Eustaquio <joaoeustaquiosg@gmail.com>
2024-03-04 15:03:38 -0300

    Updating Catalog

--- src/routes/intel/+page.svelte
+++ src/routes/intel/+page.svelte
@@ -18,11 +18,6 @@
       text: "💥 LEAGUE OF LEGENDS\n🌎 MINECRAFT\n🟥 ROBLOX\n🚔 GTA RP\n🔫 CSGO/VALORANT\n🦙 FORTNITE(Gráficos Baixos)\n\n*Processador:* I7 de 3ª Geração\n*Placa Mãe:* H61 (LGA 1155)\n*Memória RAM:* 2x8GB(16GB)\n*Air Cooler* RGB\n*Placa de Vídeo:* RX 550 4GB\n*Armazenamento:* SSD 240GB\n*Gabinete:* GAMER HAYOM C/ LED RGB\n*Fonte:* 500W\n\n*R$ 2.190,00* em até 10x sem juros!",
     },
     {
-      title: "Nº4 - i3 de 8ª + GTX 1650 4GB",
-      image: "i38.1650.png",
-      text: "💥 LEAGUE OF LEGENDS\n🌎 MINECRAFT\n🟥 ROBLOX\n🚔 GTA RP(*Gráficos Médios)\n🔫 CSGO/VALORANT\n🦙 FORTNITE(*Gráficos Médios)\n\n*Processador:* I3 DE 8ªG\n*Placa Mãe:* H310 (LGA 1200)\n*Memória RAM:* 16GB DDR4 2666Htz\n*Placa de Vídeo:* GTX 1650 4GB\n*Armazenamento:* SSD DE 512GB\n*Gabinete:* GAMER GAMER HAYOM C/3 FANS RGB + CONTROLADORA\n*Fonte:* 500W\n\n*R$ 3.330,00* em até 10x sem juros!",
-    },
-    {
       title: "Nº5 - i3 de 10ª + GTX 1650 4GB",
       image: "i312+1650.png",
       text: "💥 LEAGUE OF LEGENDS\n🌎 MINECRAFT\n🟥 ROBLOX\n🚔 GTA RP\n🔫 CSGO/VALORANT\n🦙 FORTNITE\n 🐎Red Dead Redemption 2\n🗡Elden Ring\n\n*Processador:* I3 DE 10ªG\n*Placa Mãe:* H510PM (LGA 1200)\n*Memória RAM:* 2x8GB(16GB) DDR4\n*Placa de Vídeo:* GTX 1650 4GB\n*Armazenamento:* SSD DE 480GB\n*Gabinete:* GAMER GAMER FORTREK C/3 FANS RGB\n*Fonte:* 500W\n\n*R$ 3.790,00* em até 10x sem juros!",
