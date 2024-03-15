a185f83ac8825f8336a8c87294c8ea10c3e82ce5
Joao Eustaquio <joaoeustaquiosg@gmail.com>
2024-03-15 12:46:44 -0300

    Updating i5 12

--- src/routes/intel/+page.svelte
+++ src/routes/intel/+page.svelte
@@ -25,7 +25,7 @@
     {
       title: "Nº6 - i5 de 12ª + RX 570 8GB",
       image: "I512+RX570.png",
-      text: "💥 LEAGUE OF LEGENDS\n🌎 MINECRAFT\n🟥 ROBLOX\n🚔 GTA RP\n🔫 CSGO/VALORANT\n🦙 FORTNITE\n 🐎Red Dead Redemption 2\n🗡Elden Ring\n\n*Processador:* I5-12400F (12ª Geração)\n*Placa Mãe:* H610 (LGA 1700)\n*Memória RAM:* 16GB DDR4 3200Mhz FURY BEAST RGB\n*Placa de Vídeo:* RX 570 8GB\n*Armazenamento:* NVME 256GB\n*Gabinete:* GAMER HAYOM C/ 4 FANS RGB\n*Fonte:* 500W\n\n*R$ 4.790,00* em até 10x sem juros!",
+      text: "💥 LEAGUE OF LEGENDS\n🌎 MINECRAFT\n🟥 ROBLOX\n🚔 GTA RP\n🔫 CSGO/VALORANT\n🦙 FORTNITE\n 🐎Red Dead Redemption 2\n🗡Elden Ring\n\n*Processador:* I5-12400F (12ª Geração)\n*Placa Mãe:* H610 (LGA 1700)\n*Memória RAM:* 16GB DDR4 3200Mhz FURY BEAST RGB\n*Placa de Vídeo:* GTX 1650 8GB\n*Armazenamento:* NVME 256GB\n*Gabinete:* GAMER HAYOM C/ 4 FANS RGB\n*Fonte:* 500W\n\n*R$ 4.590,00* em até 10x sem juros!",
     },
   ];
 
