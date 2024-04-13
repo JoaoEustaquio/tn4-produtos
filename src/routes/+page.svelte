7641b3a9dd9ad1cfc46377bcca5640cfe82fb67f
Joao Eustaquio <joaoeustaquiosg@gmail.com>
2024-04-13 11:52:14 -0300

    Update GPU price

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -20,7 +20,7 @@
         { title: "RX 550 4GB", price: "R$ 790,00" },
         { title: "GTX 1650 4GB VENTUS", price: "R$ 1.290,00" },
         { title: "RTX 3050 6GB", price: "R$ 1.990,00" },
-        { title: "RTX 3060 12GB", price: "R$ 2.590,00" },
+        { title: "RTX 3060 12GB", price: "R$ 2.490,00" },
         { title: "RTX 4060 8GB", price: "R$ 2.790,00" },
         { title: "RTX 4060 TI", price: "R$ 3.990,00" },
         { title: "RTX 4070 MSI WHITE", price: "R$ 5.290,00" },
