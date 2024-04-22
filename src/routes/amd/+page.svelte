69cfccf3c3ebcc74bf46a3e3a2f94674ed0b13c7
Joao Eustaquio <joaoeustaquiosg@gmail.com>
2024-04-22 17:08:28 -0300

    Adding Nº10 - Ryzen 5500 + rtx 4060ti

--- src/routes/amd/+page.svelte
+++ src/routes/amd/+page.svelte
@@ -64,6 +64,13 @@
       image: "N9-AMD.png",
       text: "🔫 WARZONE\n🔫 CSGO 2\n🦙 FORTNITE\n🪓 God Of War\n🔫 Resident Evil 4 Remake\n\n*Processador:* RYZEN 5 5600\n*Placa Mãe:* Biostar B450MX\n*Memória RAM:* 1x16GB DDR4 Fury Beast RGB 320Mhz\n*AIR Cooler* RGB\n*Placa de Vídeo:* RTX 3060 12GB\n*Armazenamento:* SSD 480GB\n*Gabinete:* GAMER TGT C/ 3 FANS Controláveis\n*Fonte:* 550W 80PLUS\n\nPOR APENAS *R$ 6.690,00* em até 10x sem juros!",
     },
+
+    // R$ 8.190,00
+    {
+      title: "Nº10 - RYZEN 5500 + RTX 4060TI 8GB",  
+      image: "N10-AMD.png",
+      text: "🔫 WARZONE\n🔫 CSGO 2\n🦙 FORTNITE\n🪓 God Of War\n🔫 Resident Evil 4 Remake\n\n*Processador:* RYZEN 5 5500\n*Placa Mãe:*  B450 ASUS - 4 BANCOS MEMORIA\n*Memória RAM:* 2x8GB DDR4 REDRAGON  3200Mhz\n*WATER COOLER 240MM*\n*Placa de Vídeo:* RTX 4060Ti 8GB INNO3D\n*Armazenamento:* NVME 512GB\n*Gabinete:* GAMER NZXT 2 FANS BLACK\n*Fonte:* 700W 80PLUS\n\nPOR APENAS *R$ 8.190,00* em até 10x sem juros!",
+    },
   ];
 
   function copyToClipboard(product: { text: string }) {
