160d6e5015fba2e48eadcf2d036a6287e1d2213a
Joao Eustaquio <joaoeustaquiosg@gmail.com>
2024-05-11 13:18:34 -0300

    AMD Pic Updated

--- src/routes/amd/+page.svelte
+++ src/routes/amd/+page.svelte
@@ -25,17 +25,17 @@
 
     // R$ 3.490,00
     {
-      title: "Nº4 - RYZEN 4500 + RX 560",
-      image: " AMD-3690.png",
-      text: "*UMA BASE DO QUE RODARIA NO COMPUTADOR:* 💥 FREE FIRE \n 🌎 MINECRAFT \n 🟥 ROBLOX \n 🚔 GTA RP \n 🔫 CSGO/VALORANT \n🦙 FORTNITE\n\n*Processador:* RYZEN 5 4500\n*Placa Mãe:* A520\n*Memória RAM:* 2x8GB MANCER 2666Mhz DDR4\n*Placa de Vídeo:* GTX 1650 4GB\n*Armazenamento:* SSD 480GB\n*Gabinete:* GAMER Hellix RGB\n*Fonte:* 500W\n\nPOR APENAS *R$ 3.790,00* em até 10x sem juros!",
-    },    
-    
-    // R$ 3.790,00
-    {
       title: "Nº5 - RYZEN 4500 + GTX 1650",
       image: "AMD-3490.png",
       text: "*UMA BASE DO QUE RODARIA NO COMPUTADOR:* \n 🔫 VALORANT\n🔫 WARZONE\n🔫 CSGO 2\n🦙 FORTNITE\n\n*Processador:* RYZEN 5 4500\n*Placa Mãe:* A520\n*Memória RAM:* 2x8GB 2666Mhz DDR4 (16GB)\n*Placa de Vídeo:* RX 560 4GB\n*Armazenamento:* SSD 240GB\n*Gabinete:* GAMER HAYOM C/3FANS RGB\n*Fonte:* 500W\n\nPOR APENAS *R$ 3.490,00* em até 10x sem juros!",
     },
+
+    // R$ 3.790,00
+    {
+      title: "Nº4 - RYZEN 4500 + RX 560",
+      image: " AMD-3790.png",
+      text: "*UMA BASE DO QUE RODARIA NO COMPUTADOR:* 💥 FREE FIRE \n 🌎 MINECRAFT \n 🟥 ROBLOX \n 🚔 GTA RP \n 🔫 CSGO/VALORANT \n🦙 FORTNITE\n\n*Processador:* RYZEN 5 4500\n*Placa Mãe:* A520\n*Memória RAM:* 2x8GB MANCER 2666Mhz DDR4\n*Placa de Vídeo:* GTX 1650 4GB\n*Armazenamento:* SSD 480GB\n*Gabinete:* GAMER Hellix RGB\n*Fonte:* 500W\n\nPOR APENAS *R$ 3.790,00* em até 10x sem juros!",
+    },    
 
     // R$ 4.590,00
     {
@@ -68,7 +68,7 @@
     // R$ 5.690,00
     {
       title: "Nº10 - RYZEN 4500 + 1650 4GB WHITE",  
-      image: "AMD-N5690.png",
+      image: "AMD-5690.png",
       text: "*UMA BASE DO QUE RODARIA NO COMPUTADOR:* \n  💥 LEAGUE OF LEGENDS\n🌎 MINECRAFT\n🟥 ROBLOX\n🔫 VALORANT\n🔫 WARZONE\n🔫 CSGO 2\n🦙 FORTNITE\n\n*Processador:* RYZEN 5 4500\n*Placa Mãe:* B550 DUEX\n*Memória RAM:* 2X8GB DDR4 RISE MODE 320Mhz\n*Water Cooler* Rise Mode Aura ICE RGB C/ VISOR LCD PERSONALIZAVEL*\n*Placa de Vídeo:* GTX 1650 4GB\n*Armazenamento:* SSD 480GB\n*Gabinete:* GAMER Fortrek Cruiser C/ 3 FANS Controláveis\n*Fonte:* 500W *BRANCA*\n\nPOR APENAS *R$ 5.690,00* em até 10x sem juros!",
     },
 
