77cef10bd3a3716913af2a139400c26bc206ff75
Joao Eustaquio <joaoeustaquiosg@gmail.com>
2024-08-27 10:01:24 -0300

    Intel Nº8 UPDATED

--- src/routes/intel/+page.svelte
+++ src/routes/intel/+page.svelte
@@ -54,7 +54,7 @@
     // R$4.690,00
     {
       title: "Nº8 - i5 de 10ª + GTX 1650 4GB",
-      image: "INTEL-4690.png",
+      image: "INTEL-4390.png",
       text: "*UMA BASE DO QUE RODARIA NO COMPUTADOR:* \n 🚔 GTA RP\n🔫 CSGO/VALORANT\n🦙 FORTNITE\n 🐎Red Dead Redemption 2\n🗡Elden Ring\n\n*Processador:* I5-10400F (10ª Geração)\n*Placa Mãe:* B560-PRO MSI (LGA 1200)\n*Memória RAM:* 2x8GB DDR4 (16GB)\n*Placa de Vídeo:* GTX 1650 4GB\n*Armazenamento:* SSD 480GB\n*Gabinete:* HAYOM BRANCO C/3 FANS\n*Fonte:* 500W\n\n*R$ 4.390,00* em até 10x sem juros!\n\n*Formas de Pagamento:*\n💳Cartão - Débito á vista ou Crédito em até *10x SEM JUROS*\n❖ Pix á Vista\n❌*NÃO* é possível realizar pagamento via Boleto Bancário",
     },
 
