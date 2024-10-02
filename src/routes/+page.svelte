0ef4a6cf9a8717abcb45eb5caa0b68ca675bb282
Joao Eustaquio <joaoeustaquiosg@gmail.com>
2024-10-02 13:09:56 -0300

    Monitor Blucase 19" code UPDATED

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
     import IconCopy from "$lib/icons/copy.svelte"
     
     const monitors = [
-        { title: "BLUECASE 19' POL. 75Hz (LC0042)", price: "R$ R$590,00" },
+        { title: "BLUECASE 19' POL. 75Hz (LC0020)", price: "R$ R$590,00" },
         { title: "VXPRO 21,5' POL. 75Hz (LC0019)", price: "R$ R$690,00" },
         { title: "3GREEN 24' POL. 75Hz (LC0014)", price: "R$ R$890,00" },
         { title: "GRASEP LED 23.8', 100HZ, 1MS, (LC0021)", price: "R$ R$1.050,00" },
