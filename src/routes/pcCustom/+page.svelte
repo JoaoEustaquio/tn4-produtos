ab9e8a65ec63566c03f49d0fe913142760aeeb34
Joao Eustaquio <joaoeustaquiosg@gmail.com>
2024-10-04 15:49:18 -0300

    PC Custom CREATED

---
+++ src/routes/pcCustom/+page.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+  import CopyButton from "$lib/CopyButton.svelte"
+
+  const products = [
+    // R$3.440,00
+    {
+      title: "",
+      image: "",
+      text: ""
+    },
+  ]
+
+  function copyToClipboard(product: { text: string }) {
+    navigator.clipboard.writeText(product.text)
+  }
+</script>
+
+<div class="p-8">
+  <a href="/"> ⬅ Home</a>
+  <span> | </span>
+  <a href="/intel">PC</a>
+  <h1 class="text-xl font-bold mb-4">🖌 PCs Personalizados</h1>
+  <div class="grid grid-cols-4 gap-8">
+    {#each products as product}
+      <div class="space-y-4">
+        <img
+          src={product.image}
+          class="rounded-md w-full h-64 object-contain border border-gray-200 bg-gray-50"
+          alt={product.title}
+        />
+        <h1 class="font-bold text-xl truncate">{product.title}</h1>
+        <code
+          class="font-mono-emoji block w-full h-64 overflow-y-scroll whitespace-pre-line bg-gray-100 border border-gray-300 p-2 rounded"
+        >
+          {product.text}
+        </code>
+
+        <CopyButton
+          class="w-full tracking-wider text-sm"
+          on:click={() => copyToClipboard(product)}>COPIAR</CopyButton
+        >
+      </div>
+    {/each}
+  </div>
+</div>
