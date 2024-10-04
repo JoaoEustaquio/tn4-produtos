8a08d31f3f5fb45e92fddf9d3fd7c43e449d2b53
Joao Eustaquio <joaoeustaquiosg@gmail.com>
2024-10-04 16:21:26 -0300

    PC AMD ADDED to PC Custom

--- src/routes/pcCustom/+page.svelte
+++ src/routes/pcCustom/+page.svelte
@@ -22,20 +22,28 @@
       image: "AMD-5990.png",
       text: "*UMA BASE DO QUE RODARIA NO COMPUTADOR:* \n💥 GTA RP\n🌎 MINECRAFT\n🔫 VALORANT\n🔫 WARZONE\n🚔 GTA 5/RP\n🔫 CSGO 2\n🦙 FORTNITE\n\n*Processador:* RYZEN 7 3700X\n*Placa Mãe:* DUEX B550ZG WHITE\n*Memória RAM:* 2x8GB 3200MHZ White RGB DDR4(16GB)\n*Placa de Vídeo:* RX 580 8GB\n*Water Cooler*: GAMDIAS AURA GL120 White\n*Armazenamento:* SSD M.2 512GB\n*Gabinete:* PCYES BOLTER WHITE GHOST\n*Fonte:* 500W *WHITE*\n\nPOR APENAS *R$ 5.990,00* em até 10x sem juros!\n\n*Formas de Pagamento:*\n💳Cartão - Débito á vista ou Crédito em até *10x SEM JUROS*\n❖ Pix á Vista\n❌*NÃO* é possível realizar pagamento via Boleto Bancário",
     },
+    
+    // R$ 7.590,00
+    {
+      title: "Nº4 - Ryzen 5600 + RTX 3060 12GB",
+      image: "FORCEFIELD.png",
+      text: "*UMA BASE DO QUE RODARIA NO COMPUTADOR:* \n  💥 LEAGUE OF LEGENDS\n🌎 MINECRAFT\n🟥 ROBLOX\n🔫 VALORANT\n🔫 WARZONE\n🚔 GTA 5/RP\n🔫 CSGO 2\n🦙 FORTNITE\n🐎Red Dead Redemption 2\n🗡Elden Ring\n\n*Processador:* RYZEN 5 5600\n*Placa Mãe:* B550M AORUS ELITE\n*Memória RAM:* 16GB DDR4 3200Mhz\n*Water Cooler* Gandias AURA 120mm V2\n*Placa de Vídeo:* RTX 3060 12GB\n*Armazenamento:* SSD NVME 512GB\n*Gabinete:* PCYES FORCEFIELD RED MAGMA C/8FANS\n*Fonte:* 600W 80Plus FULL MODULAR\n\nPOR APENAS *R$ 7.590,00* em até 10x sem juros!\n\n*Formas de Pagamento:*\n💳Cartão - Débito á vista ou Crédito em até *10x SEM JUROS*\n❖ Pix á Vista\n❌*NÃO* é possível realizar pagamento via Boleto Bancário",
+    },
 
     // R$ 8.490,00
     {
-      title: "Nº4 - Ryzen 5700X + RTX 4060 ",
+      title: "Nº5 - Ryzen 5700X + RTX 4060 ",
       image: "RYZEN5700X.png",
       text: "*UMA BASE DO QUE RODARIA NO COMPUTADOR:* \n 🔫 WARZONE\n🔫 CSGO 2\n🦙 FORTNITE\n🪓 God Of War\n🚔 GTA 5/RP\n🔫 Resident Evil 4 Remake\n\n*Processador:* RYZEN 7 5700X\n*Placa Mãe:*  B450M STEEL LEGEND\n*Memória RAM:* 2x8GB DDR4 3200Mhz\n*WATER COOLER GAMDIAS AURA 240MM*\n*Placa de Vídeo:* RTX 4060 8GB\n*Armazenamento:* NVME 512GB\n*Gabinete:* GAMER PCYES FORCEFIELD BLACK VULCAN *C/6 FANS*\n*Fonte:* 600W 80PLUS\n\nPOR APENAS *R$ 8.490,00* em até 10x sem juros!\n\n*Formas de Pagamento:*\n💳Cartão - Débito á vista ou Crédito em até *10x SEM JUROS*\n❖ Pix á Vista\n❌*NÃO* é possível realizar pagamento via Boleto Bancário",
     },
 
     // R$8.890,00
     {
-      title: "Nº5 - (PC0006)i5 de 12ª + RTX 4060 8GB ",
+      title: "Nº6 - (PC0006)i5 de 12ª + RTX 4060 8GB ",
       image: "PC0006.png",
       text: "*UMA BASE DO QUE RODARIA NO COMPUTADOR:* \n 🚘FORZA HORIZON\n🚔 GTA RP\n🔫 CSGO/VALORANT\n🦙 FORTNITE\n 🐎Red Dead Redemption 2\n🗡Elden Ring\n\n*Processador:* I5-12400F (12ª Geração)\n*Placa Mãe:*ASROCK B660M PHANTOM GAMING (LGA 1700)\n*Memória RAM:* 1x16GB DDR4 3200Mhz FURY BEAST RGB\n*AIR COOLER* GAMDIAS BOREAS E1 RGB\n*Placa de Vídeo*: RTX 4060 GALAX 8GB 1-CLICK 2X\n*Armazenamento:* SSD 512GB REDRAGON\n*Gabinete:* GAMER AQUÁRIO C/ 4 FANS RGB\n*Fonte:* 700W\n\n*R$ 7.590,00* em até 10x sem juros!\n\n*Formas de Pagamento:*\n💳Cartão - Débito á vista ou Crédito em até *10x SEM JUROS*\n❖ Pix á Vista\n❌*NÃO* é possível realizar pagamento via Boleto Bancário",
     },
+    
   ];
 
   function copyToClipboard(product: { text: string }) {
