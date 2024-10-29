1c5aaaa10358d74339fde92fa28293a56c16b522
Joao Eustaquio <joaoeustaquiosg@gmail.com>
2024-10-29 10:34:29 -0300

    PC Intel PC0020 PIC CHANGED

--- src/routes/intel/+page.svelte
+++ src/routes/intel/+page.svelte
@@ -26,7 +26,7 @@
     // R$2.190,00
     {
       title: "Nº4 - (PC0020)i7 de 3ª + RX 550  - R$2.190,00",
-      image: "N3-Intel.png",
+      image: "PC0020.png",
       text: "*UMA BASE DO QUE RODARIA NO COMPUTADOR:* \n  LEAGUE OF LEGENDS\n🌎 MINECRAFT\n🟥 ROBLOX\n🚔 GTA RP\n🔫 CSGO/VALORANT\n🦙 FORTNITE\n\n*Processador:* I7 de 3ª Geração\n*Placa Mãe:* H61 (LGA 1155)\n*Memória RAM:* 2x8GB(16GB)\n*Air Cooler* RGB\n*Placa de Vídeo:* RX 550 4GB\n*Armazenamento:* SSD 240GB\n*Gabinete:* GAMER HAYOM C/ LED RGB\n*Fonte:* 500W\n\n*R$ 2.290,00* em até 10x sem juros!\n\n*Formas de Pagamento:*\n💳Cartão - Débito á vista ou Crédito em até *10x SEM JUROS*\n❖ Pix á Vista\n❌*NÃO* é possível realizar pagamento via Boleto Bancário",
     },
 
