3f1d8ed7e4c38905e3de7fdfcb0e0b7c5c2c0e41
Joao Eustaquio <joaoeustaquiosg@gmail.com>
2024-11-18 15:23:38 -0300

    PC Custom catalog UPDATED

--- src/routes/pcCustom/+page.svelte
+++ src/routes/pcCustom/+page.svelte
@@ -2,14 +2,7 @@
   import CopyButton from "$lib/CopyButton.svelte";
 
   const products = [
-    // R$ 4.990,00
-    {
-      title: "Nº1 - Ryzen 4500 + RX 580 ",
-      image: "AMD-N7.png",
-      text: "*Máquina ideal para Jogos como:* \n  💥 LEAGUE OF LEGENDS\n🌎 MINECRAFT\n🟥 ROBLOX\n🔫 VALORANT\n🔫 WARZONE\n🚔 GTA 5/RP\n🔫 CSGO 2\n🦙 FORTNITE\n 🐎Red Dead Redemption 2\n🗡Elden Ring\n\n*Processador:* RYZEN 5 4500\n*Placa Mãe:* B450MHP\n*Memória RAM:* 2x8GB (16GB) DDR4 Redragon Solar RGB 3200MHZ\n*Placa de Vídeo:* RX 580 8GB\n*Armazenamento:* SSD 480GB\n*Gabinete:* BRX HAVOC\n*Fonte:* 500W 80Plus RGB\n\nPOR APENAS *R$ 4.990,00* em até 10x sem juros!\n\n*FFormas de Pagamento:*\n💳 Cartão - Débito à vista ou Crédito em até 10x *SEM JUROS*\n❖ Pix á Vista\n❌ NÃO aceitamos pagamento via boleto bancário\n🔥 Não perca essa chance de melhorar seu setup e jogar com qualidade! 🔥\n\n",
-    },
-
-     // R$ 5.390,00
+    // R$ 5.390,00
      {
       title: "Nº2 - (PC0059) i5 de 12ªG + RX 580",
       image: "Custom5390.png",
@@ -23,25 +16,11 @@
       text: "*Máquina ideal para Jogos como:* \n 🚔 GTA RP\n🔫 CSGO/VALORANT\n🦙 FORTNITE\n 🐎Red Dead Redemption 2\n🗡Elden Ring\n\n*Processador:* I5-10400F (10ª Geração)\n*Placa Mãe:* B560-PRO MSI (LGA 1200)\n*Memória RAM:* 2x8GB DDR4 FURY BEAST 3200MHZ (16GB)\n*Placa de Vídeo:* RTX 3050 6GB\n*Armazenamento:* SSD 480GB\n*Gabinete:* STORM-Z SHARP\n*Fonte:* 500W PFV ATIVO\n\n*R$ 6.390,00* em até 10x sem juros!\n\n*FFormas de Pagamento:*\n💳 Cartão - Débito à vista ou Crédito em até 10x *SEM JUROS*\n❖ Pix á Vista\n❌ NÃO aceitamos pagamento via boleto bancário\n🔥 Não perca essa chance de melhorar seu setup e jogar com qualidade! 🔥\n\n",
     },
 
-    // R$ 6.490,00
-    {
-      title: "Nº4 - (PC0036) PC GAMER RYZEN 5600 + RTX 3050",
-      image: "PC0036.png",
-      text: "*Máquina ideal para Jogos como:* \n  💥 LEAGUE OF LEGENDS\n🌎 MINECRAFT\n🟥 ROBLOX\n🔫 VALORANT\n🔫 WARZONE\n🚔 GTA 5/RP\n🔫 CSGO 2\n🦙 FORTNITE\n🐎Red Dead Redemption 2\n🗡Elden Ring\n\n*Processador:* RYZEN 5 5600\n*Placa Mãe:* B450MX-S\n*Memória RAM:* 2x8 XPG DG50 BRANCA RGB(16GB) DDR4\n*Water Cooler:* EVOLUT LUMINI FULL RGB WHITE\n*Placa de Vídeo:* RTX 3050 6GB\n*Armazenamento:* SSD NVME 512GB\n*Gabinete:* GAMER PCYES BOLTER WHITE GHOST C/3FANS RGB\n*Fonte:* 500W WHITE\n\nPOR APENAS *R$ 6.490,00* em até 10x sem juros!\n\n*FFormas de Pagamento:*\n💳 Cartão - Débito à vista ou Crédito em até 10x *SEM JUROS*\n❖ Pix á Vista\n❌ NÃO aceitamos pagamento via boleto bancário\n🔥 Não perca essa chance de melhorar seu setup e jogar com qualidade! 🔥\n\n",
-    },
-
     // R$ 7.590,00
     {
       title: "Nº5 - Ryzen 5600 + RTX 3060 12GB",
       image: "FORCEFIELD.png",
       text: "*Máquina ideal para Jogos como:* \n  💥 LEAGUE OF LEGENDS\n🌎 MINECRAFT\n🟥 ROBLOX\n🔫 VALORANT\n🔫 WARZONE\n🚔 GTA 5/RP\n🔫 CSGO 2\n🦙 FORTNITE\n🐎Red Dead Redemption 2\n🗡Elden Ring\n\n*Processador:* RYZEN 5 5600\n*Placa Mãe:* B550M AORUS ELITE\n*Memória RAM:* 16GB DDR4 3200Mhz\n*Water Cooler* Gandias AURA 120mm V2\n*Placa de Vídeo:* RTX 3060 12GB\n*Armazenamento:* SSD NVME 512GB\n*Gabinete:* PCYES FORCEFIELD RED MAGMA C/8FANS\n*Fonte:* 600W 80Plus FULL MODULAR\n\nPOR APENAS *R$ 7.590,00* em até 10x sem juros!\n\n*FFormas de Pagamento:*\n💳 Cartão - Débito à vista ou Crédito em até 10x *SEM JUROS*\n❖ Pix á Vista\n❌ NÃO aceitamos pagamento via boleto bancário\n🔥 Não perca essa chance de melhorar seu setup e jogar com qualidade! 🔥\n\n",
-    },
-
-    // R$ 8.490,00
-    {
-      title: "Nº6 - Ryzen 5700X + RTX 4060 ",
-      image: "RYZEN5700X.png",
-      text: "*Máquina ideal para Jogos como:* \n 🔫 WARZONE\n🔫 CSGO 2\n🦙 FORTNITE\n🪓 God Of War\n🚔 GTA 5/RP\n🔫 Resident Evil 4 Remake\n\n*Processador:* RYZEN 7 5700X\n*Placa Mãe:*  B450M STEEL LEGEND\n*Memória RAM:* 2x8GB DDR4 3200Mhz\n*WATER COOLER GAMDIAS AURA 240MM*\n*Placa de Vídeo:* RTX 4060 8GB\n*Armazenamento:* NVME 512GB\n*Gabinete:* GAMER PCYES FORCEFIELD BLACK VULCAN *C/6 FANS*\n*Fonte:* 600W 80PLUS\n\nPOR APENAS *R$ 8.490,00* em até 10x sem juros!\n\n*FFormas de Pagamento:*\n💳 Cartão - Débito à vista ou Crédito em até 10x *SEM JUROS*\n❖ Pix á Vista\n❌ NÃO aceitamos pagamento via boleto bancário\n🔥 Não perca essa chance de melhorar seu setup e jogar com qualidade! 🔥\n\n",
     },
 
     // R$ 8.590,00
