00d004584b95cd408fb3e02f70504080404ddaaf
Joao Eustaquio <joaoeustaquiosg@gmail.com>
2024-12-02 10:14:04 -0300

    New Custom Setup ADDED on catalog

--- src/routes/setupCustom/+page.svelte
+++ src/routes/setupCustom/+page.svelte
@@ -2,7 +2,7 @@
   import CopyButton from "$lib/CopyButton.svelte"
 
   const products = [
-    // R$3.440,00
+    // R$3.890,00
     {
       title: "Nº1 - () SETUP GAMER PINK - RYZE 4600G",
       image: "Pink.png",
@@ -16,11 +16,18 @@
       text: "*Setup ideal para Jogos como:* \n 💥 LEAGUE OF LEGENDS\n🌎 MINECRAFT\n🟥 ROBLOX\n🔫 VALORANT\n🔫 CSGO 2\n🔫 WARZONE (GRÁFICOS Médio/Baixos)\n🦙 FORTNITE\n\n*Placa Mãe:* A520\n*Processador:* RYZEN 5 5600G\n*Memória RAM:* 2x8GB(16GB) DDR4\n*Placa de Vídeo:*  VEGA 7 (INTEGRADO)\n*Armazenamento:* SSD 240GB\n*Gabinete:* GAMER C/ 4FANS RGB\n*Fonte:* 500W\n+ 🖥️ Monitor de 21,5' Pol.\n+ Kit Gamer (Teclado, Mouse, Mousepad e Headset)\n\nPOR APENAS *R$ 4.190,00* em até 10x sem juros!\n\n*Formas de Pagamento:*\n💳 Cartão - Débito à vista ou Crédito em até 10x *SEM JUROS*\n❖ Pix á Vista\n❌ NÃO aceitamos pagamento via boleto bancário\n🔥 Não perca essa chance de melhorar seu setup e jogar com qualidade! 🔥\n\n"
     },
 
-    // R$4.190,00
+    // R$8.090,00
     {
       title: "Nº3 - (PC0047) SETUP WHITE Ryzen 5600 + RTX 3050 GALAX 6GB",
       image: "WHITE.png",
       text: "*Setup ideal para Jogos como:*\n🚘FORZA HORIZON\n🚔 GTA 5/RP\n🔫 WARZONE/BLACK OPS 6\n🦙 FORTNITE\n🐎Red Dead Redemption 2\n\n*Placa Mãe:* DUEX B550ZGS\n*Processador:* RYZEN 5 5600\n*Memória RAM:* 2x8GB DDR4 3200Mhz PCYES UDIMM BRANCA\n*Placa de Vídeo:* RTX 3050 GALAX 6GB\n*Armazenamento:* SSD 240GB\n *Water Cooler:* PCYES SANGE FRIO3 BRANCO 120mm\n*Gabinete:* PCYES FORCEFIELD WHITE GHOST C/ 8FANS RGB\n*Fonte:* 500W BRANCA\n+ 🖥️ Monitor de 19' Pol. BRANCO\n+ Kit Gamer (Teclado, Mouse, Mousepad e Headset)\n\nPOR APENAS *R$ 8.090,00* em até 10x sem juros!\n\n*Formas de Pagamento:*\n💳 Cartão - Débito à vista ou Crédito em até 10x *SEM JUROS*\n❖ Pix á Vista\n❌ NÃO aceitamos pagamento via boleto bancário\n🔥 Não perca essa chance de melhorar seu setup e jogar com qualidade! 🔥\n\n"
+    },
+
+    // R$8.390,00
+    {
+      title: "Nº4 - () SETUP i5 de 10º(10400F) + RTX 3050 E GABINETE STORM-Z",
+      image: ".png",
+      text: "*Setup ideal para Jogos como:*\n🚔 GTA 5/RP\n\n🔫 CSGO 2\VALORANT\n🦙 FORTNITE\n🐎Red Dead Redemption 2\n🗡Elden Ring\n\n*Placa Mãe:* B560-PRO MSI(LGA 1200)\n*Processador:* I5-10400F(10º GERAÇÃO)\n*Memória RAM:* 2x8GB DDR4 FURY BEAST 3200MHZ *(16GB)*\n*Placa de Vídeo:* RTX 3050 GALAX 6GB\n*Armazenamento:* SSD 480GB\n *AIR Cooler:* REVENGE DUAL FAN RGB \n*Gabinete:* STORM-Z SHARP\n*Fonte:* 500W PFV ATIVO\n+ 🖥️ Monitor  Duex 32' 75hz PLANO Borda Infinita\n+ Kit Mecânico Knup (Teclado Mecânico Switch Azul, Mouse 4800DPI, Headset KP-359, Mousepad XL)\n\nPOR APENAS *R$ 8.090,00* em até 10x sem juros!\n\n*Formas de Pagamento:*\n💳 Cartão - Débito à vista ou Crédito em até 10x *SEM JUROS*\n❖ Pix á Vista\n❌ NÃO aceitamos pagamento via boleto bancário\n🔥 Não perca essa chance de melhorar seu setup e jogar com qualidade! 🔥\n\n"
     },
   ]
 
