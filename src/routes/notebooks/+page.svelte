261f3ec5212246ad16e2fede244c7d9f180ab3ea
Joao Eustaquio <joaoeustaquiosg@gmail.com>
2025-01-07 13:24:19 -0300

    Laptop NT0043 ADDED to catalog

--- src/routes/notebooks/+page.svelte
+++ src/routes/notebooks/+page.svelte
@@ -25,20 +25,26 @@
       text: "💻 NOTEBOOK LENOVO THINKPAD T450\n\n*Processador:* i5-5300U(5ªGEN)\n*Memória RAM:* 8GB DDR3\n*Armazenamento:* SSD 256GB\n(Acompanha Fonte)\n\n*POR APENAS R$1.390,00* em até 10x sem juros!",
     },
 
+    // R$2.290,00
+    {
+      title: "Nº3 - (NT0043)NOTEBOOK LENOVO THINKPAD T480",
+      image: "NT0043.png",
+      text: "💻 NOTEBOOK LENOVO THINKPAD T480\n\n*Processador:* i5-8550U (8ªGEN)\n*Memória RAM:* 8GB DDR4\n*Armazenamento:* SSD 256GB\n(Acompanha Fonte)\n\n*POR APENAS R$2.290,00* em até 10x sem juros!",
+    },
+
     // R$2.590,00
     {
-      title: "Nº3 - (NT0046)DELL I7-6 - 8GB - SSD256",
+      title: "Nº4 - (NT0046)DELL I7-6 - 8GB - SSD256",
       image: "NT0046.png",
       text: "💻 NOTEBOOK DELL LATITUDE\n\n*Processador:* i7 6ª GERAÇÃO\n*Memória RAM:* 8GB\n*Armazenamento:* SSD 256GB\n\nDE ~R$3.390,00~ *POR APENAS R$2.590,00* em até 10x sem juros!\n\n*Formas de Pagamento:*\n💳 Cartão - Débito à vista ou Crédito em até 10x *SEM JUROS*\n❖ Pix á Vista\n❌ NÃO aceitamos pagamento via boleto bancário\n❌ NÃO geramos link de pagamento",
     },
 
     // R$2.690,00
     {
-      title: "Nº4 - (NT0064)NOTEBOOK LENOVO IDEAPAD ",
+      title: "Nº5 - (NT0064)NOTEBOOK LENOVO IDEAPAD ",
       image: "NT0064.png",
       text: "💻 NOTEBOOK LENOVO IDEAPAD\n\n*Processador:* i7 8ª GERAÇÃO\n*Memória RAM:* 8GB DDR4\n*Armazenamento:* SSD 240GB\n(Acompanha Fonte)\n\nDE ~R$3.290,00~ POR APENAS *R$2.690,00* em até 10x sem juros!\n\n*Formas de Pagamento:*\n💳 Cartão - Débito à vista ou Crédito em até 10x *SEM JUROS*\n❖ Pix á Vista\n❌ NÃO aceitamos pagamento via boleto bancário\n❌ NÃO geramos link de pagamento",
     },
-
   ];
 
   function copyToClipboard(product: { text: string }) {
