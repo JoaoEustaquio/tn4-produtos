dd8e32c4abfd7ac0f45faa0ddb4c5714be7cd309
Joao Eustaquio <joaoeustaquiosg@gmail.com>
2025-01-27 11:24:38 -0300

    Laptop catalog UPDATED

--- src/routes/notebooks/+page.svelte
+++ src/routes/notebooks/+page.svelte
@@ -25,23 +25,16 @@
       text: "💻 NOTEBOOK LENOVO THINKPAD T480\n\n*Processador:* i5-8550U (8ªGEN)\n*Memória RAM:* 8GB DDR4\n*Armazenamento:* SSD 256GB\n(Acompanha Fonte)\n\n*POR APENAS R$2.290,00* em até 10x sem juros!",
     },
 
-    // R$2.590,00
-    {
-      title: "Nº3 - (NT0046)DELL I7-6 - 8GB - SSD256",
-      image: "NT0046.png",
-      text: "💻 NOTEBOOK DELL LATITUDE\n\n*Processador:* i7 6ª GERAÇÃO\n*Memória RAM:* 8GB\n*Armazenamento:* SSD 256GB\n\nDE ~R$3.390,00~ *POR APENAS R$2.590,00* em até 10x sem juros!\n\n*Formas de Pagamento:*\n💳 Cartão - Débito à vista ou Crédito em até 10x *SEM JUROS*\n❖ Pix á Vista\n❌ NÃO aceitamos pagamento via boleto bancário\n❌ NÃO geramos link de pagamento",
-    },
-
     // R$2.690,00
     {
-      title: "Nº4 - (NT0048)NOTEBOOK LENOVO THINKPAD T490",
+      title: "Nº3 - (NT0048)NOTEBOOK LENOVO THINKPAD T490",
       image: ".png",
       text: "💻 NOTEBOOK LENOVO THINKPAD\n\n*Processador:* i7-8665U(8ªGEN)\n*Memória RAM:* 16GB DDR4\n*Armazenamento:* SSD 240GB\n(Acompanha Fonte)\n\nPOR APENAS *R$2.690,00* em até 10x sem juros!\n\n*Formas de Pagamento:*\n💳 Cartão - Débito à vista ou Crédito em até 10x *SEM JUROS*\n❖ Pix á Vista\n❌ NÃO aceitamos pagamento via boleto bancário\n❌ NÃO geramos link de pagamento",
     },
 
     // R$2.690,00
     {
-      title: "Nº5 - (NT0064)NOTEBOOK LENOVO IDEAPAD ",
+      title: "Nº4 - (NT0064)NOTEBOOK LENOVO IDEAPAD ",
       image: "NT0064.png",
       text: "💻 NOTEBOOK LENOVO IDEAPAD\n\n*Processador:* i7 8ª GERAÇÃO\n*Memória RAM:* 8GB DDR4\n*Armazenamento:* SSD 240GB\n(Acompanha Fonte)\n\nDE ~R$3.290,00~ POR APENAS *R$2.690,00* em até 10x sem juros!\n\n*Formas de Pagamento:*\n💳 Cartão - Débito à vista ou Crédito em até 10x *SEM JUROS*\n❖ Pix á Vista\n❌ NÃO aceitamos pagamento via boleto bancário\n❌ NÃO geramos link de pagamento",
     },
