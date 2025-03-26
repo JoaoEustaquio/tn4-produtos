c2aeda4bfae863aa9d22b2fd785f1f7bab471610
Joao Eustaquio <joaoeustaquiosg@gmail.com>
2025-03-26 09:15:32 -0300

    Update RAM component listings to include DDR3 and DDR4 memory modules

--- src/routes/hardware/ram/+page.svelte
+++ src/routes/hardware/ram/+page.svelte
@@ -2,55 +2,57 @@
   import CopyButton from "$lib/CopyButton.svelte";
   import IconCopy from "$lib/icons/copy.svelte";
 
-  const intel = [
-    { code: "PO0002", title: "Intel Core I5-3470 (3ªGEN)", price: "R$220,00" },
-    { code: "PO0133", title: "Intel Core I7-3770 (3ªGEN)", price: "R$570,00" },
-    { code: "PO0122", title: "Intel Core I7-4770 (4ªGEN)", price: "R$590,00" },
-    { code: "PO0075", title: "Intel Core I3-10100F (10ªGEN)", price: "R$790,00" },
-    { code: "PO0029", title: "Intel Core I5-9600K (9ªGEN)", price: "R$850,00" },
-    { code: "PO0069", title: "Intel Core I5-10400F (10ªGEN)", price: "R$990,00" },
-    { code: "PO0031", title: "Intel Core I7-8700 (8ªGEN)", price: "R$1.190,00" },
-    { code: "PO0072", title: "Intel Core I7-6700 (6ªGEN)", price: "R$1.290,00" },
-    { code: "PO0078", title: "Intel Core I5-12400F (12ªGEN)", price: "R$1.390,00" },
-    { code: "PO0032", title: "Intel Core I7-9700 (9ªGEN)", price: "R$1.690,00" },
-];
+  const ddr3 = [
+    {
+      code: "ME0005",
+      title: "KINGSTON BLISTER 8GB 1600MHZ  VERDE DDR3 ",
+      price: "R$150,00",
+    },
+  ];
 
-  const amd = [
-    { code: "PO0040", title: "ATHLON 3000G", price: "R$600,00" },
-    { code: "PO0035", title: "AMD Ryzen 5 4500", price: "R$890,00" },
-    { code: "PO0058", title: "AMD Ryzen 5 5500", price: "R$990,00" },
-    { code: "PO0056B", title: "AMD Ryzen 5 5600G", price: "R$1.250,00" },
-    { code: "PO0057B", title: "AMD Ryzen 5 5600", price: "R$1.390,00" },
-    { code: "PO0055", title: "AMD Ryzen 5 5600GT", price: "R$1.490,00" },
-    { code: "PO0050", title: "AMD Ryzen 5 3700X", price: "R$1.490,00" },
-    { code: "PO0063", title: "AMD Ryzen 5 5700G", price: "R$1.750,00" },
-    { code: "PO00", title: "AMD Ryzen 7 5700X", price: "R$1.890,00" },
-    { code: "PO00", title: "AMD Ryzen 7 5800X", price: "R$2.490,00" },
-    { code: "PO00", title: "AMD Ryzen 7 5700X3D", price: "R$2.590,00" },
-    { code: "PO00", title: "AMD Ryzen 9 5900X", price: "R$2.990,00" },
+  const ddr4 = [
+    { code: "ME0012H", title: "KINGSTON BLISTER 8GB 2666MHZ VERDE DDR4", price: "R$190,00" },
+    { code: "ME0041", title: "FNX BLISTER 8GB 3200MHZ PRETA DDR4", price: "R$240,00" },
+    { code: "ME0027", title: "REDRAGON RAGE 8GB 3200MHZ DDR4", price: "R$260,00" },
+    { code: "ME0040", title: "RISE MODE Z BRANCA 8GB 3200MHZ DDR4", price: "R$260,00" },
+    { code: "ME0051", title: "HIKSEMI AMOUR U10 8GB 3200MHZ DDR4", price: "R$260,00" },
+    { code: "ME0055", title: "HIKSEMI FUTURE 8GB 3200MHZ DDR4", price: "R$260,00" },
+    { code: "ME0052", title: "XPG GAMMIX D35 BRANCA 8GB 3200MHZ DDR4", price: "R$280,00" },
+    { code: "ME0056", title: "HIKSEMI AMOUR WHITE 8GB 3200MHZ DDR4", price: "R$280,00" },
+    { code: "ME0054", title: "HIKSEMI FUTURE RGB 8GB 3200MHZ DDR4", price: "R$300,00" },
+    { code: "ME0042", title: "REDRAGON MAGMA 8GB 3200MHZ RGB DDR4", price: "R$310,00" },
+    { code: "ME0054", title: "FURY BEAST BLACK 8GB 3200MHZ DDR4", price: "R$310,00" },
+    { code: "ME0027", title: "REDRAGON SOLAR 8GB 3600MHZ RGB DDR4", price: "R$350,00" },
+    { code: "ME0028", title: "REDRAGON RAGE 16GB 3200MHZ DDR4", price: "R$390,00" },
+    { code: "ME0047", title: "REDRAGON MAGMA 16GB 3200MHZ  RGB DDR4", price: "R$400,00" },
+    { code: "ME0058", title: "HIKSEMI AMOUR U10 16GB 3200MHZ  DDR4", price: "R$400,00" },
+    { code: "ME0027", title: "REDRAGON SOLAR 16GB 3600MHZ  RGB DDR4", price: "R$475,00" },
+    { code: "ME0054", title: "FURY  BEAST BLACK  16GB 3200MHZ DDR4", price: "R$490,00" },
+    { code: "ME0057", title: "HIKSEMI AKIRA RGB 16GB 3200MHZ DDR4", price: "R$540,00" },
+    { code: "ME0033", title: "REDRAGON MAGMA 32GB 3200MHZ RGB DDR4", price: "R$800,00" },
   ];
 
   function copyToClipboard(product: { text: string }) {
     navigator.clipboard.writeText(product.text);
   }
 
-  function allAmd() {
-    var finalString = "Processadores AMD:\n\n";
-    amd.forEach((Amd, i) => {
+  function allDDR4() {
+    var finalString = "Memórias DDR4:\n\n";
+    ddr4.forEach((DDR4, i) => {
       finalString +=
-        `*${Amd.title}* por *${Amd.price}*` + (i + 1 < amd.length ? "\n" : "");
+        `*${DDR4.title}* por *${DDR4.price}*` + (i + 1 < ddr4.length ? "\n" : "");
     });
 
     navigator.clipboard.writeText(finalString);
   }
 
-  function allIntel() {
-    var finalString = "Processadores Intel:\n\n";
+  function allDDR3() {
+    var finalString = "Memórias DDR3:\n\n";
 
-    intel.forEach((Intel, i) => {
+    ddr3.forEach((DDR3, i) => {
       finalString +=
-        `*${Intel.title}* por *${Intel.price}*` +
-        (i + 1 < intel.length ? "\n" : "");
+        `*${DDR3.title}* por *${DDR3.price}*` +
+        (i + 1 < ddr3.length ? "\n" : "");
     });
     navigator.clipboard.writeText(finalString);
   }
@@ -69,24 +71,24 @@
   <div class="grid grid-cols-2 gap-4 mb-8">
     <div>
       <div class="flex justify-between items-center mb-4">
-        <h1 class="text-xl font-bold">🟦INTEL</h1>
-        <CopyButton on:click={() => allIntel()} class="space-x-2">
+        <h1 class="text-xl font-bold">DDR3</h1>
+        <CopyButton on:click={() => allDDR3()} class="space-x-2">
           <IconCopy />
           <span class="text-sm tracking-wider">COPIAR TODOS</span>
         </CopyButton>
       </div>
       <table class="border border-neutral-300 w-full">
-        {#each intel as Intel}
+        {#each ddr3 as DDR3}
           <tr class="group border border-neutral-300">
             <th class="text-left border border-neutral-300 p-2 bg-neutral-100"
-              >{Intel.code} | {Intel.title}</th
+              >{DDR3.code} | {DDR3.title}</th
             >
             <td class="p-2 flex justify-between items-center">
-              <span>{Intel.price}</span>
+              <span>{DDR3.price}</span>
               <CopyButton
                 on:click={() =>
                   navigator.clipboard.writeText(
-                    `🖥 *PROCESSADOR ${Intel.title}* por *${Intel.price}*`
+                    `*MEMÓRIA ${DDR3.title}* por *${DDR3.price}*`
                   )}
                 class="text-white bg-black rounded-sm p-1"
               >
@@ -100,24 +102,24 @@
 
     <div>
       <div class="flex justify-between items-center mb-4">
-        <h1 class="text-xl font-bold">🟥AMD</h1>
-        <CopyButton on:click={() => allAmd()} class="space-x-2">
+        <h1 class="text-xl font-bold">DDR4</h1>
+        <CopyButton on:click={() => allDDR4()} class="space-x-2">
           <IconCopy />
           <span class="text-sm tracking-wider">COPIAR TODOS</span>
         </CopyButton>
       </div>
       <table class="border border-neutral-300 w-full">
-        {#each amd as Amd}
+        {#each ddr4 as DDR4}
           <tr class="border border-neutral-300">
             <th class="text-left border border-neutral-300 p-2 bg-neutral-100"
-              >{Amd.code} | {Amd.title}</th
+              >{DDR4.code} | {DDR4.title}</th
             >
             <td class="p-2 flex justify-between items-center">
-              <span>{Amd.price}</span>
+              <span>{DDR4.price}</span>
               <CopyButton
                 on:click={() =>
                   navigator.clipboard.writeText(
-                    `🎮 *${Amd.title}* por *${Amd.price}*`
+                    `MEMÓRIA *${DDR4.title}* por *${DDR4.price}*`
                   )}
                 class="text-white bg-black rounded-sm p-1"
               >
