5ca8d3e282e80fa46303f8f7c9f58d3215152fce
Joao Eustaquio <joaoeustaquiosg@gmail.com>
2025-04-07 10:27:16 -0300

    Remove outdated notebook entries and associated images from the catalog

--- src/routes/notebooks/+page.svelte
+++ src/routes/notebooks/+page.svelte
@@ -10,43 +10,13 @@
     //   image: ".png",
     //   text: "💻 NOTEBOOK ...\n\n*Processador:* \n*Memória RAM:* \n*Armazenamento:*\n(Acompanha Fonte)\n\n*POR APENAS R$0.000,00* em até 10x sem juros!",
     // },
-    
-    // R$1.490,00
-    {
-      title: "Nº1 - (NT0028)NOTEBOOK LENOVO THINKPAD T450",
-      image: "NT0028.png",
-      text: "💻 NOTEBOOK LENOVO THINKPAD T450\n\n*Processador:* i5 de 6ªGEN\n*Memória RAM:* 8GB DDR3\n*Armazenamento:* SSD NVME 120GB\n(Acompanha Fonte)\n\n*POR APENAS R$1.490,00* em até 10x sem juros!",
-    },
-
-    // R$2.290,00
-    {
-      title: "Nº2 - (NT0043)NOTEBOOK LENOVO THINKPAD T480",
-      image: "NT0043.png",
-      text: "💻 NOTEBOOK LENOVO THINKPAD T480\n\n*Processador:* i5-8550U (8ªGEN)\n*Memória RAM:* 8GB DDR4\n*Armazenamento:* SSD 256GB\n(Acompanha Fonte)\n\n*POR APENAS R$2.290,00* em até 10x sem juros!",
-    },
 
     // R$2.690,00
     {
-      title: "Nº3 - (NT0048)NOTEBOOK LENOVO THINKPAD T490",
-      image: "NT0048.png",
-      text: "💻 NOTEBOOK LENOVO THINKPAD\n\n*Processador:* i7-8665U(8ªGEN)\n*Memória RAM:* 16GB DDR4\n*Armazenamento:* SSD 240GB\n(Acompanha Fonte)\n\nPOR APENAS *R$2.690,00* em até 10x sem juros!\n\n*Formas de Pagamento:*\n💳 Cartão - Débito à vista ou Crédito em até 10x *SEM JUROS*\n❖ Pix á Vista\n❌ NÃO aceitamos pagamento via boleto bancário\n❌ NÃO geramos link de pagamento",
-    },
-
-    // R$2.690,00
-    {
-      title: "Nº4 - (NT0064)NOTEBOOK LENOVO IDEAPAD ",
+      title: "Nº1 - (NT0064)NOTEBOOK LENOVO IDEAPAD ",
       image: "NT0064.png",
       text: "💻 NOTEBOOK LENOVO IDEAPAD\n\n*Processador:* i7 8ª GERAÇÃO\n*Memória RAM:* 8GB DDR4\n*Armazenamento:* SSD 240GB\n(Acompanha Fonte)\n\nDE ~R$3.290,00~ POR APENAS *R$2.690,00* em até 10x sem juros!\n\n*Formas de Pagamento:*\n💳 Cartão - Débito à vista ou Crédito em até 10x *SEM JUROS*\n❖ Pix á Vista\n❌ NÃO aceitamos pagamento via boleto bancário\n❌ NÃO geramos link de pagamento",
     },
-
-    
-    // R$3.390,00
-    {
-      title: "Nº5 - (NT0000) NOTEBOOK DELL LATITUDE 5420",
-      image: "NT0000.png",
-      text: "💻 NOTEBOOK DELL LATITUDE 5420\n\n*Processador:* i7-11857G (11ªGEN)\n*Memória RAM:* 8GB DDR4\n*Armazenamento:* SSD 240GB\n(Acompanha Fonte)\n\nPOR APENAS *R$3.390,00* em até 10x sem juros!\n\n*Formas de Pagamento:*\n💳 Cartão - Débito à vista ou Crédito em até 10x *SEM JUROS*\n❖ Pix á Vista\n❌ NÃO aceitamos pagamento via boleto bancário\n❌ NÃO geramos link de pagamento",
-    },
-    
   ];
 
   function copyToClipboard(product: { text: string }) {
